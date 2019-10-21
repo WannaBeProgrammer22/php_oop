14038e292b027bb5787d5dcc5d32be6d13d07ba7
Jerwin <jerwinfernandez04@gmail.com>
2019-10-21 21:10:21 +0800

    Init commit

---
+++ section_1/1.what_is_class.php
@@ -0,0 +1,24 @@
+<!--  
+
+  Class is a blueprint of an object.
+  Class is a logical grouping of code with variables and functions specific to that Object.
+
+  The main purpose of the class is to organize your code nto individual components.
+
+  How to define a class.
+  1. Identify how can you organize your code into objects.
+  Ex. Users, Database, Files, Posts
+  2. Then start grouping those into classes.
+  3. Each class has variables and function into related individual objects.
+  4. One Class will holds many variables and functions but it logically represent that Object.
+
+  Think how to organize
+  Ex. User class
+  What will be the variables?
+  1. Name
+  2. Age
+  What will be the fnctions?
+  1. Set the name
+  2. Set the age
+  3. Display the name and Age
+-->
